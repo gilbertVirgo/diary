14cffaa90785655f068d9abd833672022e6f501d
gilbertVirgo <gilbertjvirgo@gmail.com>
2019-09-16 16:25:35 +0100

    Added index.html

--- backend/router.js
+++ backend/router.js
@@ -2,8 +2,33 @@ const express = require("express");
 
 const router = express.Router();
 
+const entries = [
+    {
+        "id": 1,
+        "title": "My First Entry!",
+        "body": `
+            <p>Dear diary,</p>
+            <p>It was really fun today, I went to the park and stuff.</p>
+            <p>See you next time!</p>
+            <p>Kaddubusagwa</p>
+        `
+    },
+    {
+        "id": 2,
+        "title": "First day at work",
+        "body": `
+            <p>Dear diary,</p>
+            <p>First day of work was really boring man. I wish I could get back to freelancing. </p>
+            <p>See you next time!</p>
+            <p>Kaddubusagwa</p>
+        `
+    }
+]
+
 router.get("/entry/:id", (req, res) => {
-    
+    if(entries[0] === ) {
+
+    }
 });
 
 router.put("/entry", (req, res) => {
@@ -17,3 +42,5 @@ router.patch("/entry/:id", (req, res) => {
 router.get("/entries", (req, res) => {
 
 });
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,15 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const path = require("path");
+
 app.use(express.json());
-app.use(require("cors")());
+app.use(express.static(path.join(__dirname, "..", "frontend")));
 
+app.use("/", require("./router"));
 
-
-app.get("/entry/:id", (req, res) => {
-
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, "..", "frontend", req.path));
 });
-
-app.get("/entries", (req, res) => {
-
-});
-
 
 app.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`))
